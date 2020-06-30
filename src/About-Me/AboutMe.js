b7e578f600c882a93ea89e44ce056b23b68cc43f
Nathan <natemjonas@gmail.com>
2020-06-30 13:02:04 -0400

    Update about me page

--- src/About-Me/AboutMe.js
+++ src/About-Me/AboutMe.js
@@ -1,17 +1,71 @@
 import React, { Component } from "react";
 
+var techlist = [
+  "jQuery",
+  "AJAX",
+  "Typescript",
+  "Java",
+  "C++",
+  "C#",
+  "SQL",
+  "Python",
+  "HTML",
+  "JavaScript",
+  "CSS",
+  "ASP.NET",
+  "Vue",
+  "React",
+  "Django",
+  "Windows",
+  "MacOS",
+  "Ubuntu",
+  "Chrome Dev. Tools",
+  "GitHub",
+  "GitLab",
+  "VS Code",
+  "Node",
+];
+
 export default class AboutMe extends Component {
+  state = {
+    skills: techlist,
+    filter: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({ filter: event.target.value });
+  };
+
   render() {
+    const { filter, skills } = this.state;
+
+    const filteredSkills = skills.filter((item) => {
+      return item.toLowerCase().includes(filter.toLowerCase());
+    });
+
     return (
       <div className="aboutMe">
-        <div className="aboutMeHeader">About Myself</div>
+        {/* <img src="personalpic.jpg" alt="Italian Trulli" /> */}
+        <h2 className="aboutMeHeader">Welcome to my site</h2>
         <div className="aboutMeBody">
           Hello world. I am a computer science student at the University of
           Cincinnati (UC), and I hope you enjoy this React project. Throughout
           my time at UC, I have been exposed to the digital world in the form of
-          classwork and professional experience. I love it. I hope to continue
+          classroom and professional experience. I love it. I hope to continue
           my work upon graduation (04/2021) and begin my career as a software
           developer and designer.
+        </div>
+        <h2 className="aboutMeHeader">Check out my skills...</h2>
+        <div className="searchBar">
+          <input className="searchBarModule" type="text" value={filter} onChange={this.handleChange} placeholder="Search..."/>
+        </div>
+        <div class="skillContainer">
+          {filteredSkills.map((skill) => {
+            return <div class="skill">{skill}</div>;
+          })}
+        </div>
+        <div className="test">
+
         </div>
       </div>
     );
